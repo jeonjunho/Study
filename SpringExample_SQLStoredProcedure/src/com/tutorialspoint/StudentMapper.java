424c4ba293aa1ebbf7b5127994eec0403f270a4a
JeonJunho <juno008junho@gmail.com>
2019-01-04 18:56:55 +0900

    SpringExample

---
+++ SpringExample_SQLStoredProcedure/src/com/tutorialspoint/StudentMapper.java
@@ -0,0 +1,14 @@
+package com.tutorialspoint;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StudentMapper {
+	public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Student student = new Student();
+		student.setId(rs.getInt("id"));
+		student.setName(rs.getString("name"));
+		student.setAge(rs.getInt("age"));
+		return student;
+	}
+}
