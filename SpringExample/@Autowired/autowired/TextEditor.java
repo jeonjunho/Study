0566e3e82c72f3179c50fd45ba583f53a8a1f0b3
JeonJunho <juno008junho@gmail.com>
2019-01-05 23:11:20 +0900

    Add @Autowired

---
+++ SpringExample/@Autowired/autowired/TextEditor.java
@@ -0,0 +1,27 @@
+package autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TextEditor {
+    // @Autowired
+    private SpellChecker spellChecker;
+
+    // @Autowired
+    // public void setSpellChecker(SpellChecker spellChecker) {
+    // this.spellChecker = spellChecker;
+    // }
+
+    @Autowired
+    public TextEditor(SpellChecker spellChecker) {
+        System.out.println("Inside TextEditor constructor.");
+        this.spellChecker = spellChecker;
+    }
+
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+
+    public void spellCheck() {
+        spellChecker.checkSpelling();
+    }
+}
